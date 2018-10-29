f66aac2ab4404be94cf7e4106d69bb0f232c45cf
PhilDerek <filder100@onet.pl>
2018-10-29 17:16:54 +0100

    modification of sass, css, html and javaScript files and add images, font and jsConfig

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(function() {
+    // hamburger menu animation
+    if ($(window).width() < 600) {
+        $("#hamburgerBtn").click(function() {
+            $(this).toggleClass("open").addClass("ml-auto");
+            $(".menuTop").slideToggle("slow");
+            $(this).parent("nav").css("flex-direction", "column");
+        })
+    }
+
+    // to top button animations
+   $(window).scroll(function() {
+        if ($(this).scrollTop() > 450) {
+            $(".btnToTop").css("display", "initial");
+        } else {
+            $(".btnToTop").css("display", "none");
+        };
+    })
+
+    $(".btnToTop").click(function() {
+        $(document.documentElement).scrollTop(0);
+    })
+
+    //technology animations
+    $(".stackImages > div").css("opacity", "0");
+    var homeHeight = $("header").height();
+    var aboutMeHeight = $(".aboutMeInfo").height();
+    var headlineHeight = $(".technologies > h2").height();
+    var techHeight = $("#tech1").height();
+    var techScrollPosition = homeHeight + aboutMeHeight - headlineHeight;
+    var techScrollPosition2 = techScrollPosition + 2 * headlineHeight + techHeight;
+    var techScrollPosition3 = techScrollPosition2 + 2 * techHeight;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > techScrollPosition) {
+            $(".stackImages > div:nth-of-type(1)").animate({opacity: 1}, 2500);
+            $(".stackImages > div:nth-of-type(2)").animate({opacity: 1}, 2500);
+        };
+        if ($(this).scrollTop() > techScrollPosition2) {
+            $(".stackImages > div:nth-of-type(3)").animate({opacity: 1}, 2500);
+            $(".stackImages > div:nth-of-type(4)").animate({opacity: 1}, 2500);
+        };
+        if ($(this).scrollTop() > techScrollPosition3) {
+            $(".stackImages > div:nth-of-type(5)").animate({opacity: 1}, 2500);
+            $(".stackImages > div:nth-of-type(6)").animate({opacity: 1}, 2500);
+        }
+    })
+
+    //aboutme sliding
+    $(".aboutMe > img").css("position", "relative").css("right", "110vw");
+    $(".aboutMeText").css("position", "relative").css("left", "110vw");
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= $("header").height() && $(this).scrollTop() < techScrollPosition) {
+            $(".aboutMeText").animate({
+                left: "0"
+            }, 2000);
+            $(".aboutMe > img").animate({
+                right: "0"
+            }, 2000);
+        }
+    })
+    //phone number action
+    $(".phoneIcon").click(function() {
+        $(".phoneBox > p").slideToggle();
+    })
+/*
+    //parallax moving version effect
+    $(window).scroll(function() {
+        var scrollTopValue = $(window).scrollTop();
+        var windHeight = $(window).height();
+        $(".parallaxEffect").css("background-position", "center " + (windHeight * 0.5 + scrollTopValue * -0.5) + "px");
+    })
+*/
+   //army of dots
+   /*for (var i = 0; i < Math.floor(Math.random() * 5) + 3; i++) {
+        $( "<div></div>" ).appendTo( "header" ).addClass("particleDot");
+    }    */
+})
